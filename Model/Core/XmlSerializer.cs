65a7505aa75b95667067bc015b40e86ce3b1df84
Ekaterina-Kolesova <m2408550@edu.misis.ru>
2025-06-01 11:57:24 +0300

    serealizer

--- Model/Core/XmlSerializer.cs
+++ Model/Core/XmlSerializer.cs
@@ -10,106 +10,47 @@ using System.Xml.Serialization;
 
 namespace Model.Core
 {
-    internal class XMLSerializer : Serializer
+    public partial class XMLSerializer : Serializer
     {
         protected override string Extension => throw new NotImplementedException();
 
         public override void Serialize<T>(T obj, string fileName)
         {
-            throw new NotImplementedException();
-
-            //if (String.IsNullOrEmpty(fileName)) return;
-            //SelectFile(fileName);
-            //XmlSerializer xmlSerializer = new XmlSerializer(typeof(ShelterDTO));
-            //using (FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate))
-            //{
-            //    xmlSerializer.Serialize(fs, new ShelterDTO(obj));
-            //}
+            if (String.IsNullOrEmpty(fileName)) return;
+            SelectFile(fileName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(DataBaseDTO));
+            using (FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, new DataBaseDTO(obj));
+            }
         }
 
         public override T Deserialize<T>(string fileName)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(fileName)) return null;
 
-            //if (String.IsNullOrEmpty(fileName)) return null;
-
-            //SelectFile(fileName);
-            //XmlSerializer xmlSerializer = new XmlSerializer(typeof(ShelterDTO));
-            //ShelterDTO obj;
-            //using (FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate))
-            //{
-            //    obj = xmlSerializer.Deserialize(fs) as ShelterDTO;
-            //}
-            //if (obj == null) return null;
-
-            //T shelter = (T)new Shelter(obj.Name, obj.Capacity, obj.HasOpenArea);
-            //foreach (var petDTO in obj.Pets)
-            //{
-            //    Pet pet;
-            //    if (petDTO.PetType == typeof(Cat))
-            //    {
-            //        pet = new Cat(petDTO.Name, petDTO.Age, petDTO.Weigth, petDTO.HasClaustrophobia);
-            //    }
-            //    else if (petDTO.PetType == typeof(Dog))
-            //    {
-            //        pet = new Dog(petDTO.Name, petDTO.Age, petDTO.Weigth, petDTO.HasClaustrophobia);
-            //    }
-            //    else if (petDTO.PetType == typeof(Rabbit))
-            //    {
-            //        pet = new Rabbit(petDTO.Name, petDTO.Age, petDTO.Weigth, petDTO.HasClaustrophobia);
-            //    }
-            //    else continue;
-            //    shelter.Add(pet);
-            //}
-            //return shelter;
-        }
-
-        private class DataBaseDTO
-        {
-            public ShelterDTO[] Shelters { get; set; }
-            public PetDTO[] Homeless {  get; set; } 
-        }
-
-        private class ShelterDTO
-        {
-            public string Name { get; set; }
-            public int Capacity { get; set; }
-            public bool HasOpenArea { get; set; }
-            public PetDTO[] Pets { get; set; }
-
-            public ShelterDTO() { }
-            public ShelterDTO(Shelter shelter)
-            { 
-                Name = shelter.Name;
-                Capacity = shelter.Capacity;
-                HasOpenArea = shelter.HasOpenArea;
-                Pets = new PetDTO[shelter.Pets.Length];
-                for(int k = 0; k < Pets.Length; k++)
-                {
-                    Pets[k] = new PetDTO(shelter.Pets[k]);
-                }
-            }
-
-        }
-        private class PetDTO
-        {
-            public Type PetType { get; set; }
-            public string Name { get; set; }
-            public int Age { get; set; }
-            public int Weigth { get; set; }
-            public bool InShelter { get; set; }
-            public bool HasClaustrophobia { get; set; }
-
-            public PetDTO() { }
-            public PetDTO(Pet pet)
+            SelectFile(fileName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(DataBaseDTO));
+            DataBaseDTO databaseDTO;
+            using (FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate))
             {
-                PetType = pet.GetType();
-                Name = pet.Name;
-                Age = pet.Age;
-                Weigth = pet.Weigth;
-                InShelter = pet.InShelter;
-                HasClaustrophobia = pet.HasClaustrophobia;
+                databaseDTO = xmlSerializer.Deserialize(fs) as DataBaseDTO;
             }
+            if (databaseDTO == null) return null;
+
+            var homeless = new Pet[databaseDTO.Homeless.Length];
+            for(int k = 0; k < homeless.Length; k++)
+            {
+                homeless[k] = databaseDTO.Homeless[k].ToPet();
+            }
+            var shelters = new Shelter[databaseDTO.Shelters.Length];
+            for(int k = 0;k < shelters.Length; k++)
+            {
+                shelters[k] = databaseDTO.Shelters[k].ToShelter();
+            }
+
+            T database = (T)new DataBase(shelters, homeless);
+            return database;
         }
     }
 }
